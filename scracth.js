6ef19cbb148ca769816657071a818a3f9249e92b
Aaron Janes <aaron.janes@gmail.com>
2021-12-15 09:04:23 -0700

    WIP

--- scracth.js
+++ scracth.js
@@ -1,24 +1,23 @@
 
+// accepts  unsorted args and determines if it has a duplicate 
 const areThereDuplicates = (...arg) =>{
-console.log(`arg`, arg)
+
   let leftPointer = 0 ;
 //  pointer starts on the left  and moves pointer to the right to check for a match if no match move left pointer to the right one space 
   for (let x = 1 ; x < arg.length; x++){
 
     if( arg[leftPointer] !== arg[x]){
-      leftPointer++
+      leftPointer++;
     } else if(arg[leftPointer] === arg[x]){
-     
-      console.log(`true`)
-      console.log(`arg[leftPointer]:${arg[leftPointer]} , arg[x]:${arg[x]}`,)
-      return true 
+  
+      console.log(`true`);
+      console.log(`arg[leftPointer]:${arg[leftPointer]} , arg[x]:${arg[x]}`);
+      return true ;
     } 
   }
 }
 
-
 // areThereDuplicates(1, 2 ,3 , 4 , 4 ) //true
-// areThereDuplicates(1, 2, 1, 5)
-//tru
-// areThereDuplicates(3,5,6,3,9, 1)
-areThereDuplicates("a", "b", "c" , "c" ,"z")
+// areThereDuplicates(1, 2, 1, 5) //true
+// areThereDuplicates(3,5,6,3,9, 1) // true
+areThereDuplicates("a", "b", "c" , "c" ,"z") //true 
